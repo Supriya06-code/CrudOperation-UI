77a4a97719e6aad057d97a95542601390fbd722e
SJ <supriyanjadhav2000@gmail.com>
2025-02-25 15:53:36 +0530

    comment method

--- src/Services/AxiosServices.js
+++ src/Services/AxiosServices.js
@@ -1,24 +1,23 @@
-// import React, { Component } from 'react'
-// const Axios = require('axios').defaults
-// export default class AxiosServices {
-// post(url, data, Header){
-//     return Axios.post(url, data, Header);
-// }
-// }
+
 import axios from 'axios';
 
 class AxiosServices {
+  //Post request method
   post(url, data, headers = {}) {
     return axios.post(url, data, { headers });
   }
 
+  //get request method
   get(url, IsRequired=false, Header){
     return axios.get(url, IsRequired && Header)
   }
 
+  //put request method
   put(url, data, IsRequired = false, Header){
     return axios.put(url, data, IsRequired && Header)
   }
+
+  //delete request method
   delete(url, data, IsRequired = false, Header){
     return axios.delete(url, data, IsRequired && Header)
   }
--- src/Services/CrudServices.js
+++ src/Services/CrudServices.js
@@ -1,34 +1,28 @@
 
-import Axios from "./AxiosServices"
-import Configuration from "./../Configuration/Configuration";
-import axiosService from "./AxiosServices";  // Import the instance correctly
-//const axios = new Axios();
+import Configuration from "./../Configuration/Configuration";// Importing configuration for API endpoints
+import axiosService from "./AxiosServices";  // Importing the Axios instance
 
-
-//const Config = new Configuration();
-
-// export default class CrudServices   {
-//   CreateRecord(data){
-//     console.log("data : "+ data+ " Url : "+ Configuration.CreateRecord);
-//     return axios.post(Configuration.CreateRecord, data, false)
-//   }
-// }
+// Class to handle CRUD operations using Axios service
 export default class CrudServices {
+  // Method to create a new record
     CreateRecord(data) {
       console.log("Data:", data, "Url:", Configuration.CreateRecord);
       return axiosService.post(Configuration.CreateRecord, data);
     }
 
+    // Method to read records from the API
     ReadRecord(){
         console.log("Url : ",Configuration.GetRecord)
         return axiosService.get(Configuration.GetRecord,false)
     }
 
+     // Method to update an existing record
     UpdateRecord(data){
       console.log("Url : ",Configuration.UpdateRecord)
       return axiosService.put(Configuration.UpdateRecord, data, false)
     }
     
+     // Method to delete a record based on ID
     DeleteRecord(data){
       console.log("Url : ",Configuration.DeleteRecord)
       return axiosService.delete(Configuration.DeleteRecord, {data:{id:data.id}}, false)
